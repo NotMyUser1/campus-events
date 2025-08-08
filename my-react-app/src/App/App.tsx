73044bdd8487ac392da4e146bd1dd1206e1e6d0a
Jannes F <jfeye@PC110261.localdomain>
2025-08-08 14:58:44 +0200

    tmp 3

--- my-react-app/src/App/App.tsx
+++ my-react-app/src/App/App.tsx
@@ -3,21 +3,21 @@ import Header from "../Header/Header.tsx";
 import Temp from "../Temp/Temp.tsx";
 
 function App() {
+    const setTheme = (theme: "light" | "dark") => {
+        document.documentElement.setAttribute("data-theme", theme);
+    };
+
     return (
-        <>
-            <div className="App" data-theme="dark">
-                <Header />
-                <Temp />
-                <Temp />
-                <Temp />
-                <Temp />
-                <Temp />
-                <Temp />
-                <Temp />
-                <Temp />
-                <Temp />
-            </div>
-        </>
+        <div className="App">
+            <button onClick={() => setTheme("light")}>
+                Light Mode
+            </button>
+            <button onClick={() => setTheme("dark")}>
+                Dark Mode
+            </button>
+            <Header />
+            <Temp />
+        </div>
     )
 }
 
