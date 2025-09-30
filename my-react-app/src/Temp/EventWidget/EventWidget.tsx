67ebfce9a9870b7497a5ae84ede0437cb8602bce
Jannes F <jfeye@PC110261.localdomain>
2025-09-30 16:29:36 +0200

    unwichtig

--- my-react-app/src/Temp/EventWidget/EventWidget.tsx
+++ my-react-app/src/Temp/EventWidget/EventWidget.tsx
@@ -11,7 +11,7 @@ export default function EventWidget({event}: EventWidgetProps) {
     return (
         <Link to={`/event/${event._id}`} className="event-link">
             <div className='event-widget'>
-                <img src={event.imageUrl[0]}/>
+                <img src={event.imageUrl[0]} alt='No Image'/>
                 <h2>{event.title}</h2>
                 <p>{new Date(event.date).toLocaleDateString()}</p>
             </div>
